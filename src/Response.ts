c76c6067fbc216f6ba8d0456047c02f291fb1c71
Raghav Sahai <raghav.sahai21@gmail.com>
2022-01-21 20:44:01 -0500

    add set status helper functions

--- src/Response.ts
+++ src/Response.ts
@@ -9,26 +9,30 @@ const Response = (event: StandardizedEvent, context: APIGatewayEventRequestConte
     let isBase64Encoded: Boolean = false
     let statusCode: number = 200
     let body: any = {}
-    let headers: object = {}
+    let headers: any = {}
 
     // Public response API
     // TODO: Create functions to update response functions
     const response = {
-        status: (code: number): any => statusCode = code,
+        status: (code: number) : any => setStatusCode(code),
         send: (body: any): any => {},
-        header: (key: string, value: string): any => setHeader(key, value, headers)
+        header: (key: string, value: string): any => setHeader(key, value)
+    }
+    
+    // See: https://github.com/jeremydaly/lambda-api/blob/main/lib/response.js#L72
+    const setHeader = (key: string, value: string) => {
+        let _key = key.toLowerCase(); // store as lowercase
+        let _values = value ? (Array.isArray(value) ? value : [value]) : [''];
+    
+        headers[_key] = _values
+        return headers
     }
 
-    return {}
+    const setStatusCode = (code: number) => statusCode = code
+
+    return response
 }
 
-// See: https://github.com/jeremydaly/lambda-api/blob/main/lib/response.js#L72
-const setHeader = (key: string, value: string, headers: any) => {
-    let _key = key.toLowerCase(); // store as lowercase
-    let _values = value ? (Array.isArray(value) ? value : [value]) : [''];
 
-    headers[_key] = _values
-    return headers
-}
 
 export { Response }
