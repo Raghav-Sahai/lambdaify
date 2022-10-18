e23f535b3b408c0fdea0b323bf2a72935b5eb5b6
Raghav Sahai <raghav.sahai21@gmail.com>
2022-10-17 23:43:39 -0400

    100% test coverage

---
+++ __tests__/functionalTests/albEvent.test.js
@@ -0,0 +1,256 @@
+const lambdaify = require('../../src/lambdaify');
+const albEvent = require('../fixtures/albEvent.json');
+
+describe('Functional unit tests?', () => {
+    let app;
+    beforeEach(() => {
+        app = lambdaify();
+        app.get('/', (req, res) => {
+            return res
+                .header('key', 'value')
+                .header('key2', 'value2')
+                .status(201)
+                .send({
+                    body: req.body,
+                    headers: req.headers,
+                    path: req.path,
+                    method: req.method,
+                    isBase64Encoded: req.isBase64Encoded,
+                    queryStringParameters: req.queryStringParameters,
+                    params: req.params,
+                });
+        });
+        app.post('/test/path', (req, res) => {
+            return res
+                .header('key', 'value')
+                .header('key2', 'value2')
+                .code(200)
+                .send({
+                    body: req.body,
+                    headers: req.headers,
+                    path: req.path,
+                    method: req.method,
+                    isBase64Encoded: req.isBase64Encoded,
+                    queryStringParameters: req.queryStringParameters,
+                    params: req.params,
+                });
+        });
+        app.put('/test/path/:id', (req, res) => {
+            return res
+                .header('key', 'value')
+                .header('key2', 'value2')
+                .code(400)
+                .send({
+                    body: req.body,
+                    headers: req.headers,
+                    path: req.path,
+                    method: req.method,
+                    isBase64Encoded: req.isBase64Encoded,
+                    queryStringParameters: req.queryStringParameters,
+                    params: req.params,
+                });
+        });
+        app.delete('/test/path/:id/:deleteId', (req, res) => {
+            return res
+                .header('key', 'value')
+                .header('key2', 'value2')
+                .code(200)
+                .send({
+                    body: req.body,
+                    headers: req.headers,
+                    path: req.path,
+                    method: req.method,
+                    isBase64Encoded: req.isBase64Encoded,
+                    queryStringParameters: req.queryStringParameters,
+                    params: req.params,
+                });
+        });
+    });
+    describe('Happy Paths', () => {
+        describe('When app.run is called with an alb event to an existing route (/)', () => {
+            it('then the correct response is returned', async () => {
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'GET',
+                    path: '/',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: { query: '1234' },
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: '{"body":"Hello from alb!","headers":{"accept":"application/json"},"path":"/","method":"GET","isBase64Encoded":false,"queryStringParameters":{"query":"1234"},"params":{}}',
+                    headers: {
+                        key: 'value',
+                        key2: 'value2',
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 201,
+                    statusDescription: 'Created',
+                });
+            });
+        });
+        describe('When app.run is called with an alb event to an existing route (/test/path)', () => {
+            it('then the correct response is returned', async () => {
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'POST',
+                    path: '/test/path',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: { query: '1234', query2: '12345' },
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: '{"body":"Hello from alb!","headers":{"accept":"application/json"},"path":"/test/path","method":"POST","isBase64Encoded":false,"queryStringParameters":{"query":"1234","query2":"12345"},"params":{}}',
+                    headers: {
+                        key: 'value',
+                        key2: 'value2',
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 200,
+                    statusDescription: 'OK',
+                });
+            });
+        });
+        describe('When app.run is called with an alb event to an existing route (/test/path/:id)', () => {
+            it('then the correct response is returned', async () => {
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'PUT',
+                    path: '/test/path/1234',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: {},
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: '{"body":"Hello from alb!","headers":{"accept":"application/json"},"path":"/test/path/1234","method":"PUT","isBase64Encoded":false,"queryStringParameters":{},"params":{"id":"1234"}}',
+                    headers: {
+                        key: 'value',
+                        key2: 'value2',
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 400,
+                    statusDescription: 'Bad Request',
+                });
+            });
+        });
+        describe('When app.run is called with an alb event to an existing route (/test/path/:id/:deleteId)', () => {
+            it('then the correct response is returned', async () => {
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'DELETE',
+                    path: '/test/path/1234/12345',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: {},
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: '{"body":"Hello from alb!","headers":{"accept":"application/json"},"path":"/test/path/1234/12345","method":"DELETE","isBase64Encoded":false,"queryStringParameters":{},"params":{"id":"1234","deleteId":"12345"}}',
+                    headers: {
+                        key: 'value',
+                        key2: 'value2',
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 200,
+                    statusDescription: 'OK',
+                });
+            });
+        });
+    });
+    describe('Error Paths', () => {
+        describe('When a duplicate route is being added to app', () => {
+            it('Then an error is thrown', () => {
+                expect(() => app.get('/', () => 'test')).toThrow(
+                    'GET / already exists and cannot be registered again.'
+                );
+            });
+        });
+        describe('When app.run is called with an alb event and there is no matching route', () => {
+            it('then a 404 route not found response is returned', async () => {
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'GET',
+                    path: '/imaginaryRoute',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: {},
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: 'Resource not found',
+                    headers: {
+                        'Content-Type': 'text/plain',
+                        'x-amzn-ErrorType': 404,
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 404,
+                });
+            });
+        });
+        describe('When app.run is called with an alb event and there is no matching route for the exact httpVerb', () => {
+            it('then a 404 route not found response is returned', async () => {
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'GET',
+                    path: '/test/path',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: {},
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: 'Resource not found',
+                    headers: {
+                        'Content-Type': 'text/plain',
+                        'x-amzn-ErrorType': 404,
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 404,
+                });
+            });
+        });
+        describe('When the request parameter is called with a method that does not exist', () => {
+            it('Then an error response returned', async () => {
+                app.get('/requestErrorCase', (req, _) => req.notRealProperty());
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'GET',
+                    path: '/requestErrorCase',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: {},
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: 'req.notRealProperty is not a function',
+                    headers: {
+                        'Content-Type': 'text/plain',
+                        'x-amzn-ErrorType': 500,
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 500,
+                });
+            });
+        });
+        describe('When the response parameter is called with a method that does not exist', () => {
+            it('Then an error response returned', async () => {
+                app.get('/responseErrorCase', (_, res) =>
+                    res.notRealProperty()
+                );
+                const testAlbEvent = {
+                    ...albEvent,
+                    httpMethod: 'GET',
+                    path: '/responseErrorCase',
+                    headers: { accept: 'application/json' },
+                    queryStringParameters: {},
+                };
+                const response = await app.run(testAlbEvent, {});
+                expect(response).toStrictEqual({
+                    body: 'res.notRealProperty is not a function',
+                    headers: {
+                        'Content-Type': 'text/plain',
+                        'x-amzn-ErrorType': 500,
+                    },
+                    isBase64Encoded: false,
+                    statusCode: 500,
+                });
+            });
+        });
+    });
+});
--- src/lambdaify.ts
+++ src/lambdaify.ts
@@ -4,32 +4,24 @@ import { Response } from './Response';
 import { router } from './Router';
 
 const ROUTE_NOT_FOUND = new Error('Resource not found');
-const UNEXPECTED_ERROR = new Error('Unexpected error');
-const LAMBDAIFY_OPTIONS_ERROR = new Error(
-    'Lambdaify options must be an object'
-);
 
-const lambdaify = options => {
-    options = options || {};
-    if (typeof options !== 'object') {
-        throw LAMBDAIFY_OPTIONS_ERROR;
-    }
-
+const lambdaify = () => {
     const Router = router({});
 
     const lambdaify = {
         run: async (event, context) => {
             const matchedRoute: Route = Router.matchedRoute(event);
             if (Object.keys(matchedRoute).length === 0) {
-                console.log('HERE');
                 return formatError(ROUTE_NOT_FOUND, 404);
             }
-
             const { callback, params } = matchedRoute;
+
+            const request = new Request(event, context, params);
+            const response = new Response(event, context);
             try {
-                return await handleRun(event, context, callback, params);
+                return await handleRun(request, response, callback);
             } catch (err) {
-                return formatError(UNEXPECTED_ERROR, 500);
+                return formatError(err, 500);
             }
         },
         get: (path: string, callback) =>
@@ -46,19 +38,15 @@ const lambdaify = options => {
 };
 
 const handleRun = async (
-    event: any,
-    context: any,
-    callback: any,
-    params: Array<Param>
+    request: any,
+    response: any,
+    callback: any
 ) => {
-    const request = new Request(event, context, params);
-    const response = new Response(event, context);
-
     try {
         await callback(request, response);
         return response.createResponse();
-    } catch (err) {
-        return formatError(err, 500);
+    } catch (error) {
+        throw error;
     }
 };
 const formatError = (error: Error, statusCode: number) => {
